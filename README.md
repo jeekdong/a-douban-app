d2520eeb98e536cbc7472427c4cc84ed9409949e
lemondogs <jeekdong@gmail.com>
2017-04-03 22:31:29 +0800

    a douban app

---
+++ README.md
@@ -0,0 +1,19 @@
+# 豆瓣App
+
+> 基于Vue2.0的仿豆瓣app
+
+## 配置
+
+``` bash
+# 安装依赖
+npm install
+
+# 构建本地服务器 localhost:8080
+npm run dev
+
+# build
+npm run build
+
+# build 并且显示详细信息
+npm run build --report
+```
---
+++ src/pages/Index/Index.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<m-header title="豆瓣app" :bg="true">
+		</m-header>
+	</div>
+</template>
+
+<script>
+  import mHeader from '../../components/header'
+  export default {
+    name: 'index',
+    components: {
+      mHeader
+    }
+  }
+</script>
+
+
+<style lang="less">
+
+</style
