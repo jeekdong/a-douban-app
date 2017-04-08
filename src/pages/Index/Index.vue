84b40fe7a9aa548782ddce2996461d09a313999f
lemondogs <jeekdong@gmail.com>
2017-04-08 22:54:53 +0800

    添加m-cell组件和m-cell-media组件

--- src/pages/Index/Index.vue
+++ src/pages/Index/Index.vue
@@ -4,32 +4,23 @@
             <a href="javascript:;" slot="right">分享</a>
         </m-header>
         <div class="page-content">
-            <m-swipe swipeid="swipe01" :autoplay="1000" effect="cube">
-                <div class="swiper-slide slide02" slot="swiper-con">Slide 1</div>
-                <div class="swiper-slide slide01" slot="swiper-con">Slide 2</div>
-                <div class="swiper-slide slide03" slot="swiper-con">Slide 3</div>
+            <m-swipe swipeid="swipe01" :autoplay="1000" paginationDirection="right">
+            <div class="swiper-slide slide01" slot="swiper-con">slide 01</div>
+            <div class="swiper-slide slide01" slot="swiper-con">slide 02</div>
+            <div class="swiper-slide slide01" slot="swiper-con">slide 03</div>
             </m-swipe>
-            <m-swipe swipeid="swipe021" :loop="false" paginationType="fraction" :autoplay="2000">
-                <div class="swiper-slide slide01" slot="swiper-con">Slide 1</div>
-                <div class="swiper-slide slide02" slot="swiper-con">Slide 2</div>
-                <div class="swiper-slide slide03" slot="swiper-con">Slide 3</div>
-            </m-swipe>
-            <p>asdasdas</p>
-            <p>asdasdas</p>
-
-            <p>asdasdas</p>
-            <p>asdasdas</p>
-            <p>asdasdas</p>
-
-            <p>asdasdas</p>
-            <p>asdasdas</p>
-            <p>asdasdas</p>
-            <p>asdasdas</p>
-            <p>asdasdas</p>
-            <p>asdasdas</p>
-            <p>asdasdas</p>
-
         </div>
+        <m-cell title="提醒" icon>
+        <img src="../../assets/images/ic_mine_notification.png" slot="icon">
+        <a href="javascript:;" slot="cell-right"><img src="../../assets/images/ic_arrow_gray_small.png" alt=""></a>
+      </m-cell>
+      <m-cell title="设置">
+        <a href="javascript:;" slot="cell-right"><img src="../../assets/images/ic_arrow_gray_small.png" alt=""></a>
+      </m-cell>
+      <m-cell-media author="作者：大象公会" column="来自栏目：广播精选" img="https://qnmob2.doubanio.com/img/files/file-1489047494.jpg">
+         <span slot="title">个人意见：为什么中国没有鲍勃·迪伦这样的民谣歌手</span>
+         <span slot="describe">我们这一代人的成长年代，真正的诗歌课从来都是缺席的。</span>
+       </m-cell-media>
     </div>
 </template>
 
