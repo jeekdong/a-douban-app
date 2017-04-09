bb92c78fad6532f3560aceb24a60eb0bff32417a
lemondogs <jeekdong@gmail.com>
2017-04-09 22:41:48 +0800

    添加引入axios

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import './assets/less/app.less';
-Vue.config.productionTip = false
-
+//Vue.config.productionTip = false
+Vue.use(VueAxios,axios)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
